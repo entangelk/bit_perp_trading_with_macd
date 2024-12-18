5773b4429b3301ce2cf7aa8f2c93c7f75403087f
entagelk <kdtyohan@gmail.com>
2024-12-19 01:28:29 +0900

    update

--- docs/cal_position.py
+++ docs/cal_position.py
@@ -15,7 +15,7 @@ def cal_position(df):
     # 이전 포지션과 비교하여 변화가 있을 때만 시그널 생성
     fl_position_changed = df['fl_position'] != df['fl_position'].shift(1)
     st_position_changed = df['st_position'] != df['st_position'].shift(1)
-    hma_position_changed = df['signal_hma'] != df['signal_hma'].shift(1)
+  
     
     # 포지션이 변경된 경우에만 딕셔너리에 저장
     if fl_position_changed.iloc[-1]:
@@ -28,10 +28,9 @@ def cal_position(df):
     else:
         position_dict['super trend'] = None
 
-    if hma_position_changed.iloc[-1]:
-        position_dict['hma_signal'] = df['signal_hma'].iloc[-1]
-    else:
-        position_dict['hma_signal'] = None
+    # 연속 시그널널
+    position_dict['hma_signal'] = df['signal_hma'].iloc[-1]
+ 
     
     print(df.index[-1])
     print(f'Flow Line : {position_dict["Flow Line"]}, super trend : {position_dict["super trend"]}, HMA : {position_dict["hma_signal"]}')
@@ -55,23 +54,23 @@ def cal_position(df):
     
     # HMA 신호가 있으면 마지막으로 적용
     if position_dict['hma_signal'] is not None:
-        squeeze_signals = check_squeeze_signals(df)
+        df = check_squeeze_signals(df)
         current = df.iloc[-1]
-        
+                
         # 기본 신호 준비
         signal = position_dict['hma_signal']  # 'Long' 또는 'Short'
-                
+                        
         # 1. MACD 방향 확인
         macd_aligned = (signal == 'Long' and current['macd_diff'] > 0) or \
-                    (signal == 'Short' and current['macd_diff'] < 0)
-        
+                        (signal == 'Short' and current['macd_diff'] < 0)
+                
         # 2. 스퀴즈 방향 확인
-        momentum_aligned = (signal == 'Long' and squeeze_signals['momentum']['color'] == 'EMERALD') or \
-                        (signal == 'Short' and squeeze_signals['momentum']['color'] == 'RED')
-        
+        momentum_aligned = (signal == 'Long' and current['squeeze_color'] == 'EMERALD') or \
+                            (signal == 'Short' and current['squeeze_color'] == 'RED')
+                
         # 3. 스퀴즈 강도 확인
-        strong_squeeze = squeeze_signals['squeeze']['state'] == 'YELLOW'
-        
+        strong_squeeze = current['squeeze_state'] == 'YELLOW'
+                
         # 모든 조건 확인
         if macd_aligned and momentum_aligned and strong_squeeze:
             final_position = 'hma_' + signal
